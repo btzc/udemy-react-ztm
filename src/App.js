c2b3eed068705f8914b159dc402cc4153558b7a7
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2019-12-30 19:38:46 -0800

    react course lesson 70

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
@@ -22,8 +17,8 @@ function App() {
       />
       <Route 
         exact
-        path='/hats'
-        component={HatsPage}
+        path='/shop'
+        component={ShopPage}
       />
     </Switch>
     </div>
