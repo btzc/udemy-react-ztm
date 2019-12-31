4e843cb9cf25da333430fa47dffbccf32bbcaefe
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2019-12-31 15:53:20 -0800

    react course lesson 91

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:554048044923:web:246dd7419b418bdd5947de"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ 
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch(err) {
+      console.log('error creating user', err.message);
+    }
+  }
+
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
